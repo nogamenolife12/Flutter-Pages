8d9451f8b179f0be7bf8a8581f99757bc2745a00
vanshaj rana <vanshajrana@vanshajs-MacBook-Air-3.local>
2025-02-24 15:08:55 +0530

    added an appbar and user img+username

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_application/app%20styles/app_style.dart';
 
 class HomePage extends StatelessWidget{
   const HomePage ({super.key});
@@ -6,12 +7,33 @@ class HomePage extends StatelessWidget{
 @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: Text("This is the Home page",style: TextStyle(
-          color: Colors.white,
-          fontSize: 28,
-          fontWeight: FontWeight.bold,
-        ),),
+      
+      appBar: AppBar(
+        title: Text("Home page", style: TextStyle(
+          color: AppStyle.font,
+        ),
+        ),
+        backgroundColor: AppStyle.background,
+        actions: [
+          Icon(Icons.access_alarms_sharp,
+          color: Colors.white
+          ,)
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset('assets/temp/userimg.webp',
+              width: 40,
+              height: 40,),
+              Text("John styles",style: TextStyle(
+            color: AppStyle.font
+          ),)
+            ],
+          ),
+          
+        ],
       ),
     );
   }
